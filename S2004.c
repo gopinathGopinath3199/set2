ab3fb5d01faaf30fa94e023b7ed9c6e24fa87a32
gopinathGopinath3199 <47105587+gopinathGopinath3199@users.noreply.github.com>
2019-02-21 16:01:44 +0530

    Create S2004.c

---
+++ S2004.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,a,b;
+printf("enter the value");
+scanf("%d%d",&a,&b);
+{
+for(i=a;i<=b;i++)
+{
+if(i%2==1)
+printf("%d   ",i);
+}
+getch();
+}
+}
